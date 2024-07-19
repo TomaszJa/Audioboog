b0a20454f42c327097a6514343d616576b552bc1
Tomasz Jaszcz <t.jaszcz99@gmail.com>
2024-07-19 17:26:27 +0200

    Added tracking and forwarding/rewersing music

--- app/src/main/java/com/example/audioboog/MainActivity.java
+++ app/src/main/java/com/example/audioboog/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.audioboog;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.Manifest;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -27,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        requestPermissionLauncher.launch("android.Manifest.permission.READ_MEDIA_AUDIO");
+        listView = findViewById(R.id.listViewSong);
+        requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_AUDIO);
     }
+
     // Register the permissions callback, which handles the user's response to the
     // system permissions dialog. Save the return value, an instance of
     // ActivityResultLauncher, as an instance variable.
@@ -36,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
             registerForActivityResult(new RequestPermission(), isGranted -> {
                 if (isGranted) {
                     Log.i("Permission: ", "Granted");
+                    displaySongs();
                     // Permission is granted. Continue the action or workflow in your
                     // app.
                 } else {
@@ -47,5 +63,74 @@ public class MainActivity extends AppCompatActivity {
                     // decision.
                 }
             });
+
+    public ArrayList<File> findSongs(File file) {
+        ArrayList<File> foundSongs = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        if (files != null) {
+            for (File singlefile : files) {
+                if (singlefile.isDirectory() && !singlefile.isHidden()) {
+                    foundSongs.addAll(findSongs(singlefile));
+                } else if (singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")) {
+                    foundSongs.add(singlefile);
+                }
+            }
+        }
+        return foundSongs;
+    }
+
+    void displaySongs() {
+        final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().replace(".mp3", "").replace(".wav", "");
+        }
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+//        listView.setAdapter(myAdapter);
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String songName = listView.getItemAtPosition(position).toString();
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+                        .putExtra("songs", mySongs)
+                        .putExtra("songname", songName)
+                        .putExtra("pos", position));
+            }
+        });
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            @SuppressLint("ViewHolder") View myView = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textSong = myView.findViewById(R.id.txtsongname);
+            textSong.setSelected(true);
+            textSong.setText(items[position]);
+
+            return myView;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/audioboog/PlayerActivity.java
@@ -0,0 +1,223 @@
+package com.example.audioboog;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PlayerActivity extends AppCompatActivity {
+    Button play_button, previous_button, next_button, fast_forward_button, fast_rewind_button;
+    TextView txtsname, txtsstart, txtsstop;
+    SeekBar seekmusic;
+
+    String songName;
+    public static final String EXTRA_NAME = "song_name";
+    static MediaPlayer mediaPlayer;
+    int position;
+    ArrayList<File> mySongs;
+    Thread updateSeekBar;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_player);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        play_button = findViewById(R.id.play_button);
+        previous_button = findViewById(R.id.previous_button);
+        next_button = findViewById(R.id.next_button);
+        fast_forward_button = findViewById(R.id.fast_forward_button);
+        fast_rewind_button = findViewById(R.id.fast_rewind_button);
+        txtsname = findViewById(R.id.txtsn);
+        txtsstart = findViewById(R.id.txtsstart);
+        txtsstop = findViewById(R.id.txtsstop);
+        seekmusic = findViewById(R.id.seekbar);
+
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+
+        String selectedSongName = i.getStringExtra("songname");
+        txtsname.setSelected(true);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        if (bundle != null) {
+            mySongs = bundle.getParcelableArrayList("songs", File.class);
+            position = bundle.getInt("pos", 0);
+            Uri uri = Uri.parse(mySongs.get(position).toString());
+            songName = mySongs.get(position).getName();
+            txtsname.setText(songName);
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+        }
+
+        updateSeekBar = new Thread() {
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+
+                while (currentPosition < totalDuration) {
+                    try {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekmusic.setProgress(currentPosition);
+                    } catch (InterruptedException | IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                }
+                super.run();
+            }
+        };
+        seekmusic.setMax(mediaPlayer.getDuration());
+        updateSeekBar.start();
+        seekmusic.getProgressDrawable().setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.MULTIPLY);
+        seekmusic.getThumb().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+
+        seekmusic.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        String endTime = createTime(mediaPlayer.getDuration());
+        txtsstop.setText(endTime);
+
+        final Handler handler = new Handler();
+        final int delay = 1000;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String currentTime = createTime(mediaPlayer.getCurrentPosition());
+                txtsstart.setText(currentTime);
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+        play_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    play_button.setBackgroundResource(R.drawable.ic_play);
+                    mediaPlayer.pause();
+                } else {
+                    play_button.setBackgroundResource(R.drawable.ic_pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                position = ((position+1)%mySongs.size());
+                Uri uri = Uri.parse(mySongs.get(position).toString());
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+                songName = mySongs.get(position).getName();
+                txtsname.setText(songName);
+                mediaPlayer.start();
+                play_button.setBackgroundResource(R.drawable.ic_pause);
+            }
+        });
+        previous_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                position = ((position-1)<0)?(mySongs.size()-1):(position-1);
+                Uri uri = Uri.parse(mySongs.get(position).toString());
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+                songName = mySongs.get(position).getName();
+                txtsname.setText(songName);
+                mediaPlayer.start();
+                play_button.setBackgroundResource(R.drawable.ic_pause);
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                next_button.performClick();
+            }
+        });
+
+        fast_forward_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 10000);
+                }
+            }
+        });
+
+        fast_rewind_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 10000);
+                }
+            }
+        });
+    }
+
+    public String createTime(int duration) {
+        String time = "";
+        int min = duration/1000/60;
+        int sec = duration/1000%60;
+
+        time+=min+":";
+
+        if (sec<10) {
+            time +="0";
+        }
+        time += sec;
+
+        return time;
+    }
+}
